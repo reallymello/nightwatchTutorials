a8e6a908b7b84851a554c7071c54044acee2afd3
David Mello <theycallmedavidmello@gmail.com>
2021-01-17 01:59:49 -0500

    Fixed typos

--- a11yTestPlanExample/test/a11y-v1.js
+++ a11yTestPlanExample/test/a11y-v1.js
@@ -1,10 +1,5 @@
-/*
-Rule Explanations
-https://github.com/dequelabs/axe-core/blob/develop/doc/rule-descriptions.md
-https://dequeuniversity.com/rules/axe/4.1
-*/
 module.exports = {
-    'Run everything except contrast and region': function (browser) {
+    'Run all accessibility assertions': function (browser) {
         browser.page.news()
             .navigate()
             .axeInject()
