5395636d4ddfafb4947fcb00dfbc5a4e93b9cc17
David Mello <theycallmedavidmello@gmail.com>
2023-05-25 06:28:02 -0400

    Updated config to newer style and packages

--- dropDowns/test/calcTests1.js
+++ dropDowns/test/calcTests1.js
@@ -2,19 +2,18 @@
 // See calcTests2.js for a better pattern where we start to use the Page Object Model.
 
 module.exports = {
-    '@tags': ['bad'],
-    'hello world': function (browser) {
-        browser
-            .url('http://localhost:3000')
-            .assert.title('reallyMello calculator');
-    },
-    'two plus two is four': function (browser) {
-        browser
-            .url('http://localhost:3000')
-            .setValue('#numList1', 2)
-            .setValue('#operatorList', '+')
-            .setValue('#numList2', 2)
-            .click('#submit')
-            .expect.element('#result').text.to.equal('4');
-    }
-}
+  '@tags': ['bad'],
+  'hello world': function (browser) {
+    browser.url('http://localhost:3000').assert.title('reallyMello calculator');
+  },
+  'two plus two is four': function (browser) {
+    browser
+      .url('http://localhost:3000')
+      .setValue('#numList1', 2)
+      .setValue('#operatorList', '+')
+      .setValue('#numList2', 2)
+      .click('#submit')
+      .expect.element('#result')
+      .text.to.equal('4');
+  },
+};
--- dropDowns/test/calcTests3.js
+++ dropDowns/test/calcTests3.js
@@ -1,69 +1,74 @@
 // Final form of an optimized test class using Nightwatch.js's Page Object API
 // See calcTests1.js and calcTests2.js for incremental changes.
 // Future tutorials we'll cover optimal test case selection.
-
 module.exports = {
-    '@tags': ['best'],
-    beforeEach: function (browser) {
-        browser.page.calculator().navigate();
-    },
-    'onload welcome with addition and zeroed values': function (browser) {
-        browser
-            .assert.title('reallyMello calculator');
+  '@tags': ['best'],
+  beforeEach: function (browser) {
+    browser.page.calculator().navigate();
+  },
+  'onload welcome with addition and zeroed values': function (browser) {
+    browser.assert.title('reallyMello calculator');
 
-        browser.page.calculator()
-            .assert.containsText('@result', '')
-            .assert.containsText('@firstNumberSelected', '0')
-            .assert.containsText('@secondNumberSelected', '0')
-            .assert.containsText('@operatorSelected', '+');
-    },
-    'two plus two is four': function (browser) {
-        browser.page.calculator()
-            .add(2, 2)
-            .expect.element('@result')
-            .text.to.equal('4');
-    },
-    '5 plus 5 equals 10': function (browser) {
-        // Example of another style of writing the test
-        // which may hide too much of what is being actually tested.
-        // This may be a step too far depending on your style preference.
-        browser.page.calculator()
-            .adding(5).plus(5).equals(10);
-    },
-    '0-5=-5': function (browser) {
-        browser.page.calculator()
-            .subtract(0, 5)
-            .expect.element('@result')
-            .text.to.equal('-5');
-    },
-    '5-0=5': function (browser) {
-        browser.page.calculator()
-            .subtract(5, 0)
-            .expect.element('@result')
-            .text.to.equal('5');
-    },
-    '2-2=0': function (browser) {
-        browser.page.calculator()
-            .subtract(2, 2)
-            .expect.element('@result')
-            .text.to.equal('0');
-    },
-    '5*2=10': function (browser) {
-        browser.page.calculator()
-            .multiply(5, 2)
-            .expect.element('@result')
-            .text.to.equal('10');
-    },
-    '5 divided by 0 is Infinity': function (browser) {
-        browser.page.calculator()
-            .divide(5, 0)
-            .expect.element('@result')
-            .text.to.equal('Infinity');
-    },
-    '0/5=0': function (browser) {
-        browser.page.calculator()
-            .divide(0, 5)
-            .expect.element('@result')
-            .text.to.equal('0');
-    }
-}
+    browser.page
+      .calculator()
+      .assert.containsText('@result', '')
+      .assert.containsText('@firstNumberSelected', '0')
+      .assert.containsText('@secondNumberSelected', '0')
+      .assert.containsText('@operatorSelected', '+');
+  },
+  'two plus two is four': function (browser) {
+    browser.page
+      .calculator()
+      .add(2, 2)
+      .expect.element('@result')
+      .text.to.equal('4');
+  },
+  '5 plus 5 equals 10': function (browser) {
+    // Example of another style of writing the test
+    // which may hide too much of what is being actually tested.
+    // This may be a step too far depending on your style preference.
+    browser.page.calculator().adding(5).plus(5).equals(10);
+  },
+  '0-5=-5': function (browser) {
+    browser.page
+      .calculator()
+      .subtract(0, 5)
+      .expect.element('@result')
+      .text.to.equal('-5');
+  },
+  '5-0=5': function (browser) {
+    browser.page
+      .calculator()
+      .subtract(5, 0)
+      .expect.element('@result')
+      .text.to.equal('5');
+  },
+  '2-2=0': function (browser) {
+    browser.page
+      .calculator()
+      .subtract(2, 2)
+      .expect.element('@result')
+      .text.to.equal('0');
+  },
+  '5*2=10': function (browser) {
+    browser.page
+      .calculator()
+      .multiply(5, 2)
+      .expect.element('@result')
+      .text.to.equal('10');
+  },
+  '5 divided by 0 is Infinity': function (browser) {
+    browser.page
+      .calculator()
+      .divide(5, 0)
+      .expect.element('@result')
+      .text.to.equal('Infinity');
+  },
+  '0/5=0': function (browser) {
+    browser.page
+      .calculator()
+      .divide(0, 5)
+      .expect.element('@result')
+      .text.to.equal('0');
+  },
+};
